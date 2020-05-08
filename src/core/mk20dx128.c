36abf97870851230cd25386659b414c6889c864e
Brian Taylor <brian.taylor@bolderflight.com>
2020-05-07 18:33:28 -0600

    Added a def for _write

--- src/core/mk20dx128.c
+++ src/core/mk20dx128.c
@@ -1201,6 +1201,12 @@ int _read(int file, char *ptr, int len)
 	return 0;
 }
 
+__attribute__((weak))
+int _write(int file, char *ptr, int len)
+{
+	return 0;
+}
+
 __attribute__((weak)) 
 int _close(int fd)
 {
