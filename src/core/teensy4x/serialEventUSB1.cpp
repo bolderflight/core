07bb0217c1b96308d7d44d9cc77d75807a6fb899
Brian Taylor <brian.taylor@bolderflight.com>
2021-01-29 17:52:47 -0700

    Added Teensy 4.0 and Teensy 4.1 support

---
+++ src/core/teensy4x/serialEventUSB1.cpp
@@ -0,0 +1,6 @@
+#include <cstdint>
+#include "core/teensy4x/pgmspace.h"
+// #include <Arduino.h>
+void serialEventUSB1() __attribute__((weak));
+void serialEventUSB1() {}
+uint8_t _serialEventUSB1_default PROGMEM = 1;	
